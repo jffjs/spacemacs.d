81e567c9095f1d610648800bb2e9d9e6b7c4bc4b
jffjs <jffreyjs@gmail.com>
2015-06-05 16:58:12 -0400

    turn rust back on

--- contrib/rust/packages.el
+++ contrib/rust/packages.el
@@ -8,6 +8,9 @@
   (use-package rust-mode
     :init
     (progn
+      (setq racer-rust-src-path "/usr/local/src/rust/src/")
+      (setq racer-cmd "/usr/local/src/racer/target/release/racer")
+      (eval-after-load "rust-mode" '(require 'racer))
       (add-hook 'rust-mode-hook (lambda ()
                                   (flycheck-mode)))
       (evil-leader/set-key-for-mode 'rust-mode
